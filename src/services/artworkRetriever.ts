8c2f0b470302d3d67145a6a8af6474ddfc43ff44
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-06-25 00:13:30 +0300

    artwork rtrvr

--- src/services/artworkRetriever.ts
+++ src/services/artworkRetriever.ts
@@ -1,3 +1,13 @@
+import { requestOptions } from "./requestOptions";
+
 export const artworks =
   "https://api.artic.edu/api/v1/artworks?page=1&limit=100";
-console.log(artworks);
+
+export function artworkRetriever() {
+  const link1 =
+    "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,image_id&limit=100";
+
+  fetch(link1, requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(`this is data ${data}`));
+}
