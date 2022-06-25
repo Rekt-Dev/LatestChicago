39ac5a51d1dc81d9d0f22826d2836cb2ce1c649e
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-06-25 18:51:57 +0300

    importer

--- src/components/Importer.tsx
+++ src/components/Importer.tsx
@@ -1,7 +1,8 @@
 import { requestOptions } from "../services/requestOptions";
 
-export function artworksImporter() {
-  const link = "";
+export function Importer() {
+  const link = "https://api.artic.edu/api/v1/artworks?limit=100";
+  let imported: any;
   fetch(link, requestOptions)
     .then((response) => response.json())
     .then((data) => console.log(data));
